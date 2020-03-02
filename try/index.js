5e1e10104e3c2f2e5a4e82c7a985a05473a06db1
Sahil3738 <s.malhotra738@mybvc.ca>
2020-03-02 11:54:25 -0700

    homework

---
+++ try/index.js
@@ -0,0 +1,17 @@
+
+
+ const name = document.querySelector("#fname");
+
+
+const submit = document.querySelector("#submit");
+
+
+ const show = document.querySelector("#show");
+
+
+      submit.addEventListener("click" , function()
+    {
+        console.log(name);
+   //show.textContent= fname.Value;
+       
+    }
